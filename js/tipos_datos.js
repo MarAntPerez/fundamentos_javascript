a5c2313edf0b41fab5fddaabe2fa2a34a2d46a3b
Marco <maphy1707@gmail.com>
2024-07-08 16:11:11 -0600

    Tipos de Datos y Operadores de asignacion

---
+++ js/tipos_datos.js
@@ -0,0 +1,39 @@
+// String
+let nombre = "Juan";
+console.log(nombre);
+
+// Number
+let edad = 20;
+console.log(edad);
+
+// decimal
+let peso = 125.3998;
+console.log(peso);
+
+// Boolean
+let esEstudiante = false;
+console.log(esEstudiante);
+
+// Undefined
+let indefinido;
+
+// Nulo
+let nulo = null;
+console.log(nulo);
+
+// Symbol
+let simbolo = Symbol("miSimbolo");
+console.log(simbolo);
+
+// Concatenacion
+let saludo = "Hola " + nombre;
+console.log(saludo);
+
+// ejemplos de tipos de datos
+let contador = 0;
+contador = contador + 100;
+console.log(contador);
+
+let contadorDecimal = 1.25;
+contadorDecimal = contadorDecimal + 74.32;
+console.log(contadorDecimal);
